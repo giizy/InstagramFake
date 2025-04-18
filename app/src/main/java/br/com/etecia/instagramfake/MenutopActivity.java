b67de2b2d8cb92bbd92108e83bf1ff8e2eae47a0
giizy <giovanna.pm2020@gmail.com>
2025-04-17 22:12:05 -0300

    commit

--- app/src/main/java/br/com/etecia/instagramfake/MenutopActivity.java
+++ app/src/main/java/br/com/etecia/instagramfake/MenutopActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenutopActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
